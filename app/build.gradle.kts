9de703cd31335f5011d685886eb88b56f362cd89
Paolo Soravia <149009448+psoravia@users.noreply.github.com>
2025-10-13 11:40:38 +0200

    refactor(sonar): add missing `{` in logger message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -269,7 +269,7 @@ tasks.register("jacocoTestReport", JacocoReport::class) {
         val newContent = reportFile.readText().replace("<line[^>]+nr=\"65535\"[^>]*>".toRegex(), "")
         reportFile.writeText(newContent)
 
-        logger.quiet("Wrote summarized jacoco test coverage report xml to $reportFile.absolutePath}")
+        logger.quiet("Wrote summarized jacoco test coverage report xml to ${reportFile.absolutePath}")
     }
 
     }
