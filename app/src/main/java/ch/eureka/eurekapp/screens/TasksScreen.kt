057167326b7713041df10f445e2573dffcfe2b54
Ismail Moudden <ismail.moudden1@gmail.com>
2025-10-14 15:43:44 +0200

    refactor(screens): unify naming to TasksScreen and remove old TaskScreen duplicate; keep light preview only

--- app/src/main/java/ch/eureka/eurekapp/screens/TasksScreen.kt
+++ app/src/main/java/ch/eureka/eurekapp/screens/TasksScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
