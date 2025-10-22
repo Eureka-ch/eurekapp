84b56658a1e5a0a144c593c17950527871e3e5ef
Alessandro Lombardini <113979061+ahlombardini@users.noreply.github.com>
2025-10-22 18:15:38 +0200

    fix rebase conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.kotlinxSerialization)
-    alias(libs.plugins.kotlinxSerialization)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.ktfmt)
     alias(libs.plugins.gms)
