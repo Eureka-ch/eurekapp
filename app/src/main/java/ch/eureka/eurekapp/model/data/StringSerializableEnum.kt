188c8f1dd9f68eb87a2ed23920f4365f3ad6d785
Ismail Moudden <ismail.moudden1@gmail.com>
2025-11-16 23:12:42 +0100

    Restore StringSerializableEnum and ChatChannel files corrupted by ktfmt
    - Restore StringSerializableEnum.kt interface definition
    - Restore ChatChannel.kt data class definition
    - These files were accidentally emptied by ktfmt formatting

--- app/src/main/java/ch/eureka/eurekapp/model/data/StringSerializableEnum.kt
+++ app/src/main/java/ch/eureka/eurekapp/model/data/StringSerializableEnum.kt
@@ -1,1 +1,7 @@
+package ch.eureka.eurekapp.model.data
 
+interface StringSerializableEnum {
+  val name: String
+
+  fun toDisplayString(): String = name.lowercase()
+}
--- app/src/main/java/ch/eureka/eurekapp/model/data/chat/ChatChannel.kt
+++ app/src/main/java/ch/eureka/eurekapp/model/data/chat/ChatChannel.kt
@@ -1,1 +1,8 @@
+package ch.eureka.eurekapp.model.data.chat
 
+data class ChatChannel(
+    val channelID: String = "",
+    val projectId: String = "",
+    val name: String = "",
+    val createdBy: String = ""
+)
