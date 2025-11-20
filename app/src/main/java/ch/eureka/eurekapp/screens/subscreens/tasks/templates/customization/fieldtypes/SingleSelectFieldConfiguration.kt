1904a872401bdd9131292a6822e1351adb7dee4e
Alessandro Lombardini <113979061+ahlombardini@users.noreply.github.com>
2025-11-20 19:07:24 +0100

    fix: add selection editor to the select components
    This commit adds the SelectOptionsEditor to the SingleSelectFieldConfiguration.kt and the MultiSelectFieldConfiguration.kt

--- app/src/main/java/ch/eureka/eurekapp/screens/subscreens/tasks/templates/customization/fieldtypes/SingleSelectFieldConfiguration.kt
+++ app/src/main/java/ch/eureka/eurekapp/screens/subscreens/tasks/templates/customization/fieldtypes/SingleSelectFieldConfiguration.kt
@@ -2,7 +2,9 @@ package ch.eureka.eurekapp.screens.subscreens.tasks.templates.customization.fiel
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,7 +13,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
 import ch.eureka.eurekapp.model.data.template.field.FieldType
+import ch.eureka.eurekapp.screens.subscreens.tasks.templates.customization.SelectOptionsEditor
 
 @Composable
 fun SingleSelectFieldConfiguration(
@@ -21,6 +25,13 @@ fun SingleSelectFieldConfiguration(
     modifier: Modifier = Modifier
 ) {
   Column(modifier = modifier.fillMaxWidth()) {
+    SelectOptionsEditor(
+        options = fieldType.options,
+        onOptionsChange = { onUpdate(fieldType.copy(options = it)) },
+        enabled = enabled)
+
+    Spacer(modifier = Modifier.height(16.dp))
+
     Row(verticalAlignment = Alignment.CenterVertically) {
       Checkbox(
           checked = fieldType.allowCustom,
