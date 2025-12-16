e2ba622442729a2700acd87a613b6282571a04a3
Ismail Moudden <ismail.moudden1@gmail.com>
2025-12-16 15:18:42 +0100

    Fix compilation error: Remove incorrect waitUntil import
    - Remove incorrect import of waitUntil (it's an extension function, not a direct import)
    - All Android tests and unit tests now compile successfully

--- app/src/androidTest/java/ch/eureka/eurekapp/screen/ViewTaskScreenTest.kt
+++ app/src/androidTest/java/ch/eureka/eurekapp/screen/ViewTaskScreenTest.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
-import androidx.compose.ui.test.waitUntil
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
