e90d4effb315406c96cabc4ec3b230deeebd3a95
Ismail Moudden <ismail.moudden1@gmail.com>
2025-12-17 12:44:42 +0100

    chore: apply ktfmt formatting

--- app/src/main/java/ch/eureka/eurekapp/ui/ideas/IdeasContent.kt
+++ app/src/main/java/ch/eureka/eurekapp/ui/ideas/IdeasContent.kt
@@ -39,7 +39,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
