02100a35133f1b2c82567e13cc4f57bcccda65bd
Ismail Moudden <ismail.moudden1@gmail.com>
2025-12-17 19:49:57 +0100

    Run ktfmt after nav updates

--- app/src/main/java/ch/eureka/eurekapp/navigation/Navigation.kt
+++ app/src/main/java/ch/eureka/eurekapp/navigation/Navigation.kt
@@ -4,7 +4,6 @@ package ch.eureka.eurekapp.navigation
 import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
