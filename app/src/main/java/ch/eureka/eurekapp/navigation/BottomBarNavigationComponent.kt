88ca51679f942bf021b1179333082c7633607d38
Ismail Moudden <ismail.moudden1@gmail.com>
2025-12-17 20:09:20 +0100

    Added missing import

--- app/src/main/java/ch/eureka/eurekapp/navigation/BottomBarNavigationComponent.kt
+++ app/src/main/java/ch/eureka/eurekapp/navigation/BottomBarNavigationComponent.kt
@@ -20,6 +20,7 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.AssignmentTurnedIn
 import androidx.compose.material.icons.filled.CalendarToday
 import androidx.compose.material.icons.filled.Folder
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Lightbulb
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.AssignmentTurnedIn
